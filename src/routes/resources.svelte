dbad9f028ec145608305ad43f2d4d006a75eca54
duckRabbitPy <duck.rabbit.python@gmail.com>
2021-10-10 22:39:11 +0100

    update resource links

--- src/routes/resources.svelte
+++ src/routes/resources.svelte
@@ -1,66 +1,97 @@
 <h1>Resources</h1>
 
 <ul>
-	<h2>Journals, articles and bulletins</h2>
-	<li>
-		<a href="https://www.frontiersin.org/articles/10.3389/fpsyg.2016.01792/full"
-			>Underlying neuroscience: review article</a
-		>
-	</li>
-	<li>
-		<a
-			href="https://www.fil.ion.ucl.ac.uk/~karl/Predictions,%20perception,%20and%20a%20sense%20of%20self.pdf"
-			>Sense of self and agency: review article</a
-		>
-	</li>
-	<li>
-		<a href="https://www.mindbrained.org/october-2020-predictive-processing/"
-			>Mind Brain Ed Bulletin</a
-		>
-	</li>
-	<li>
-		<a
-			href="https://philosophyofbrains.com/2014/06/22/is-prediction-error-minimization-all-there-is-to-the-mind.aspx"
-			>Prediction error minimisation</a
-		>
-	</li>
-	<li>
-		<a
-			href="https://www.researchgate.net/publication/237085110_The_Computational_Anatomy_of_Psychosis"
-			>Computational models of psychosis</a
-		>
-	</li>
-	<li>
-		<a
-			href="https://www.fil.ion.ucl.ac.uk/~karl/The%20free-energy%20principle%20-%20a%20rough%20guide%20to%20the%20brain.pdf"
-			>The Free energy principle</a
-		>
-	</li>
-	<li>
-		<a href="https://elifesciences.org/articles/53588">Perceptual inference and vision</a>
-	</li>
+  <h2>Journals, articles and bulletins</h2>
+  <li>
+    <a href="https://www.frontiersin.org/articles/10.3389/fpsyg.2016.01792/full"
+      >Underlying neuroscience: review article</a
+    >
+  </li>
+  <li>
+    <a
+      href="https://www.fil.ion.ucl.ac.uk/~karl/Predictions,%20perception,%20and%20a%20sense%20of%20self.pdf"
+      >Sense of self and agency: review article</a
+    >
+  </li>
+  <li>
+    <a href="https://www.mindbrained.org/october-2020-predictive-processing/"
+      >Mind Brain Ed Bulletin</a
+    >
+  </li>
+  <li>
+    <a
+      href="https://philosophyofbrains.com/2014/06/22/is-prediction-error-minimization-all-there-is-to-the-mind.aspx"
+      >Prediction error minimisation</a
+    >
+  </li>
+  <li>
+    <a
+      href="https://www.researchgate.net/publication/237085110_The_Computational_Anatomy_of_Psychosis"
+      >Computational models of psychosis</a
+    >
+  </li>
+  <li>
+    <a
+      href="https://www.fil.ion.ucl.ac.uk/~karl/The%20free-energy%20principle%20-%20a%20rough%20guide%20to%20the%20brain.pdf"
+      >The Free energy principle</a
+    >
+  </li>
+  <li>
+    <a href="https://elifesciences.org/articles/53588"
+      >Perceptual inference and vision</a
+    >
+  </li>
 </ul>
 
 <ul>
-	<h2>Websites</h2>
-	<li>
-		<a href="https://predictive-mind.net/@@kw2paper?keyword=Predictive%20processing"
-			>Predictive-mind</a
-		>
-	</li>
-	<li>
-		<a href="https://www.x-spect.org/">X-SPECT</a>
-	</li>
+  <h2>Websites</h2>
+  <li>
+    <a
+      href="https://predictive-mind.net/@@kw2paper?keyword=Predictive%20processing"
+      >Predictive-mind</a
+    >
+  </li>
+  <li>
+    <a href="https://www.x-spect.org/">X-SPECT</a>
+  </li>
+</ul>
+
+<ul>
+  <h2>Books</h2>
+  <li>
+    <a href="https://www.goodreads.com/book/show/25823558-surfing-uncertainty"
+      >Surfing Uncertainty - Andy Clark</a
+    >
+  </li>
+  <li>
+    <a href="https://www.goodreads.com/book/show/18012311-the-predictive-mind"
+      >The Predictive Mind - Jakob Howhy</a
+    >
+  </li>
+  <li>
+    <a href="https://www.goodreads.com/book/show/56018879-of-sound-mind"
+      >Of Sound Mind: How Our Brain Constructs a Meaningful Sonic World - Nina
+      Kraus</a
+    >
+  </li>
+  <li>
+    <a href="https://www.goodreads.com/book/show/34540099-experienced-wholeness"
+      >Experienced Wholeness: Integrating Insights from Gestalt Theory,
+      Cognitive Neuroscience, and Predictive Processing - Wanja Wiese</a
+    >
+  </li>
+  <li>
+    <a href="https://www.goodreads.com/book/show/38633773-the-spontaneous-brain"
+      >The Spontaneous Brain: From the Mind-Body to the World-Brain Problem -
+      Georg Northoff</a
+    >
+  </li>
 </ul>
 
 <style>
-	h1,
-	h2,
-	h3,
-	h4,
-	h5,
-	p,
-	a {
-		margin-left: 2.5rem;
-	}
+  h1,
+  h2,
+  a {
+    margin-left: 2.5rem;
+  }
 </style>
